397779b63372236f04a3e2c81812e55b32e3cadb
jespermunkeby <46975845+jespermunkeby@users.noreply.github.com>
2020-12-31 16:53:04 +0100

    Implemented and tested EdgeIdentity

--- _test.py
+++ _test.py
@@ -1,6 +1,7 @@
 import unittest
-from main import Node, LinkedLine, Grid
+from main import Node, LinkedLine, Grid, Orientation, EdgeIdentity
 import numpy as np
+from copy import deepcopy
 
 class TestLinkedLine(unittest.TestCase):
     def test_adaptive_subdivision(self):
@@ -21,6 +22,26 @@ class TestLinkedLine(unittest.TestCase):
         l.adaptive_subdivision(0.24)
 
         self.assertEqual(sum([1 for each in l]), 9)
+    
+    def test_translate(self):
+        l = LinkedLine()
+        l.add(0,0)
+        l.add(1,0)
+        l.translate([0,1])
+
+        self.assertEqual(str(l), "(1,1) -> (0,1)")
+
+        l.translate([-15,3])
+
+        self.assertEqual(str(l), "(-14,4) -> (-15,4)")
+    
+    def test_rotate(self):
+        l = LinkedLine()
+        l.add(0,0)
+        l.add(1,0)
+        l.rotate(np.pi)
+
+        self.assertAlmostEqual(l[0].pos[0],-1)     
 
 class TestGrid(unittest.TestCase):
     def test_insert_line(self):
@@ -58,12 +79,61 @@ class TestGrid(unittest.TestCase):
         node1 = None
         node2 = None
         for nd in l:
-            if nd.data.tolist() == [1.5,1.5]:
+            if nd.pos.tolist() == [1.5,1.5]:
                 node1 = nd
                 node2 = node1.next
 
         self.assertEqual(len(g.get_neighbourhood(node1)),8) #Middle tile, searching in 9 tiles excluding itself
         self.assertEqual(len(g.get_neighbourhood(node2)),5)
+
+class TestOrientation(unittest.TestCase):
+    def test_iter_oriented(self):
+        l = LinkedLine()
+
+        pos = [[0,0],[1,0],[1,1]]
+        for p in pos:
+            l.add(p[0],p[1])
+
+        o = Orientation()
+        for i,node in enumerate(o.iter_oriented(l)):
+            a = pos[::-1][i]
+            b = node.pos.tolist()
+
+            self.assertAlmostEqual(a[0],b[0])
+            self.assertAlmostEqual(a[1],b[1])
+
+class TestEdgeIdentity(unittest.TestCase):
+    def test___iter__(self):
+        l = LinkedLine()
+        l.add(0,0)
+        l.add(1,0)
+
+        ors_data = [[0,[0,0]],[np.pi,[1,1]],[0,[2,3]]]
+        ors = [Orientation(*o) for o in ors_data]
+
+        ei = EdgeIdentity(l,ors)
+            
+        for i,nodes in enumerate(ei):
+            
+            node1, node2, node3 = nodes
+
+            #test node1
+            self.assertAlmostEqual(node1.pos[0],l[i].pos[0])
+            self.assertAlmostEqual(node1.pos[1],l[i].pos[1])
+
+            #test node2
+            l_copy = deepcopy(l)
+            l_copy.rotate(ors_data[1][0])
+            l_copy.translate(ors_data[1][1])
+            self.assertAlmostEqual(node2.pos[0],l_copy[i].pos[0])
+
+            #...
+
+
+
+            
+
+
     
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 from random import random
+from copy import deepcopy
 
 class Node:
     def __init__(self, x, y):
-        self.data = np.array([x,y])
+        self.pos = np.array([x,y])
         self.next = None
         self.prev = None
     
@@ -11,15 +12,15 @@ class Node:
         return self.next != None
 
     def dist_to_next(self):
-        return np.linalg.norm(self.data - self.next.data)
+        return np.linalg.norm(self.pos - self.next.pos)
 
     def __str__(self):
-        return f"({self.data[0]},{self.data[1]})"
+        return f"({self.pos[0]},{self.pos[1]})"
     
 
 class LinkedLine:
     def __init__(self):
-        self.head = None        
+        self.head = None     
 
     def add( self, x,y ) :
         node = Node(x,y)
@@ -45,17 +46,30 @@ class LinkedLine:
         while node != None:
             yield node
             node = node.next
+    
+    def __getitem__(self, n):       
+        return next(x for i,x in enumerate(self) if i==n)
 
     def adaptive_subdivision(self, max_dist):
         for node in self:
             if node.has_next():
                 while node.dist_to_next() > max_dist:
-                    x, y =  ((node.data + node.next.data)/2).tolist()
+                    x, y =  ((node.pos + node.next.pos)/2).tolist()
                     new_node = Node(x,y)
                     new_node.next = node.next
                     new_node.prev = node
                     node.next = new_node
-
+    
+    def translate(self, vec):
+        for node in self:
+            node.pos += vec
+    
+    def rotate(self, theta):
+        c, s = np.cos(theta), np.sin(theta)
+        rotmat = np.array(((c, -s), (s, c)))
+        for node in self:
+            node.pos = np.matmul(rotmat, node.pos)
+        
 
 class Grid:
     def __init__(self, gridsize):
@@ -66,7 +80,7 @@ class Grid:
         self.ht = {}
     
     def hash_node(self,node):
-        return tuple(np.floor(node.data // self.gridsize).tolist())
+        return tuple(np.floor(node.pos // self.gridsize).tolist())
     
     def insert_line(self,line):
         for node in line:
@@ -95,3 +109,63 @@ class Grid:
         for key, val in self:
             s += f"{key}: {val} \n"
         return s
+
+class Orientation:
+    def __init__(self, rotation = 0, translatement = [0,0]):
+        self.rotation = rotation
+        self.translatement = translatement
+    
+    def iter_oriented(self, identity):
+        orientation = deepcopy(identity)
+        orientation.rotate(self.rotation)
+        orientation.translate(self.translatement)
+
+        for node in orientation:
+            yield node
+
+
+class EdgeIdentity: #Line along with its orientations
+    def __init__(self, identity, orientations):
+        self.identity = identity
+        self.orientations = orientations
+    
+    def __iter__(self):
+        """
+            Iterates through oriented copies of identity.
+            Multipresent nodes are returned in a list, in which all
+            nodes are in some sense equivalent
+        """
+        l = []
+        for ori in self.orientations:
+            nodes = []
+            for node in ori.iter_oriented(self.identity):
+                nodes.append(node)
+            l.append(nodes)
+            
+        for nodes in zip(*l):
+            yield nodes
+    
+
+class OrganicPenrose:
+    def __init__(self):
+        self.kite = []
+        
+    
+    def get_dart_forces(self, l1, l2, l3):
+        #1. Copy l1, l2, l3 into edges edges
+        pass
+    
+    def get_kite_forces(self, l1, l2, l3):
+        pass
+    
+    def grow(self,prob):
+        pass
+    
+    def subdivide(self,max_dist):
+        pass
+
+    def differentiate(self):
+        pass
+
+    def update(self):
+        pass
